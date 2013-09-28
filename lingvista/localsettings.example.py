13bda07d5cd75208c3d1993c00f0869ea7d94b59
Michael Elovskikh <wronglink@gmail.com>
2013-09-28 10:04:15 +0600

    Bootstraped settings

---
+++ lingvista/localsettings.example.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+SECRET_KEY = '@mo#3hjd#_x5bn0x0@43px-7(frjlebr2=$^bu*unzk4nd4z$4'
+
+ALLOWED_HOSTS = []
