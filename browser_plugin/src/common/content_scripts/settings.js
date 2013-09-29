78ef4eaebb9205555feec96835e39ff64d6b39f3
Michael Elovskikh <wronglink@gmail.com>
2013-09-30 00:25:01 +0600

    Your last searches

--- browser_plugin/src/common/content_scripts/settings.js
+++ browser_plugin/src/common/content_scripts/settings.js
@@ -1,6 +1,6 @@
 var Settings = {
-    "URL_STATIC": "http://localhost:8000/static/",
-    "URL_TRANSDEF_API": "http://localhost:8000/api/v1/transdef/",
-    "URL_LANGUAGES_API": "http://localhost:8000/api/v1/languages/",
-    "URL_ACCOUNT_API": "http://localhost:8000/api/v1/account/"
+    "URL_STATIC": "http://lingvista.org/static/",
+    "URL_TRANSDEF_API": "http://lingvista.org/api/v1/transdef/",
+    "URL_LANGUAGES_API": "http://lingvista.org/api/v1/languages/",
+    "URL_ACCOUNT_API": "http://lingvista.org/api/v1/account/"
 };
