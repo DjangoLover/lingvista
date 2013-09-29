4b26ed523ddc67365ffe2994f7e3ea420180fc4e
Michael Elovskikh <wronglink@gmail.com>
2013-09-29 11:25:52 +0600

    Small registration fixes

--- lingvista/urls.py
+++ lingvista/urls.py
@@ -7,6 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^api/v1/', include('lingvista.api.urls')),
     url(r'^$', 'lingvista.views.index', name='lingvista-index'),
-    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
