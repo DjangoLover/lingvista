7f4b04d235eafa4b945ec23f1399d1798ac559bc
Michael Elovskikh <wronglink@gmail.com>
2013-09-29 14:22:34 +0600

    TransdefLog model changes

--- lingvista/transdef/models.py
+++ lingvista/transdef/models.py
@@ -7,11 +7,11 @@ class TransDefLog(models.Model):
     Model stores user search for translation or definition
     """
     source = models.TextField('Source text', max_length='200')
-    text = models.TextField('Translation or definiton', max_length='500')
-    is_definition = models.BooleanField()
-    is_translation = models.BooleanField()
+    translation = models.TextField('Translation', max_length='500')
+    definition = models.TextField('Definiton', max_length='500')
     lang_from = models.ForeignKey('Language', name='Source language', related_name='source_set')
     lang_to = models.ForeignKey('Language', name='Target language', related_name='transdef_set')
+    created_at = models.DateTimeField(auto_created=True)
 
     def __unicode__(self):
         return '{0}: {1}'.format(self.text[:50], self.score)
